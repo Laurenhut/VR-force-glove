f07a5573f7a25dd772f0ef2fc78012ab07e9ca5b
unknown <l.hutson@live.com>
2018-11-09 03:41:40 -0600

    added calibration step for the imu and binds the imu values to be between 0 and 1. Added the motor lock/ unlock to the arduino code. Changed the servo library to pwm servo due to interfearance with the bluetooth. Removed imu value binding from python.

--- arduino/bluetooth_with_imu/bluetooth_with_imu.ino
+++ arduino/bluetooth_with_imu/bluetooth_with_imu.ino
@@ -13,11 +13,15 @@
 #include "MPU9250.h"
 #define AHRS true         // Set to false for basic data read
 #include <SoftwareSerial.h>  
+#include <PWMServo.h>
+PWMServo myservo;  // create servo object to control a servo
 
+int pos = 0;    // variable to store the servo position
 int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
 int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
 int blue = 0;
 float value = 0.5;
+
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 // Pin definitions
@@ -31,6 +35,8 @@ MPU9250 myIMU;
 
 void setup()
 {
+  myservo.attach(SERVO_PIN_A);  // attaches the servo on pin 9 to the servo object
+  myservo.write(45); 
   Wire.begin();
   Serial.begin(9600);
 
@@ -54,12 +60,12 @@ void setup()
 
   // Read the WHO_AM_I register, this is a good test of communication
   byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
-  Serial.print("MPU9250 "); Serial.print("I AM "); Serial.print(c, HEX);
-  Serial.print(" I should be "); Serial.println(0x71, HEX);
+//  Serial.print("MPU9250 "); Serial.print("I AM "); Serial.print(c, HEX);
+//  Serial.print(" I should be "); Serial.println(0x71, HEX);
 
   if (c == 0x71) // WHO_AM_I should always be 0x68
   {
-    Serial.println("MPU9250 is online...");
+//    Serial.println("MPU9250 is online...");
 
     // Calibrate gyro and accelerometers, load biases in bias registers
     myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
@@ -67,17 +73,19 @@ void setup()
     myIMU.initMPU9250();
     // Initialize device for active mode read of acclerometer, gyroscope, and
     // temperature
-    Serial.println("MPU9250 initialized for active data mode....");
+//    Serial.println("MPU9250 initialized for active data mode....");
 
     // Read the WHO_AM_I register of the magnetometer, this is a good test of
     // communication
     byte d = myIMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
-    Serial.print("AK8963 "); Serial.print("I AM "); Serial.print(d, HEX);
-    Serial.print(" I should be "); Serial.println(0x48, HEX);
+//    Serial.print("AK8963 "); Serial.print("I AM "); Serial.print(d, HEX);
+//    Serial.print(" I should be "); Serial.println(0x48, HEX);
     // Get magnetometer calibration from AK8963 ROM
     myIMU.initAK8963(myIMU.magCalibration);
     // Initialize device for active mode read of magnetometer
-    Serial.println("AK8963 initialized for active data mode....");
+//    Serial.println("AK8963 initialized for active data mode....");
+    //Serial.println(0);
+
    
   } // if (c == 0x71)
   else
@@ -86,6 +94,9 @@ void setup()
     Serial.println(c, HEX);
     while(1) ; // Loop forever if communication doesn't happen
   }
+
+  // initializes the xervo on pin 9 and put it into the freewheeling position
+
 }
 
 float force_sensor_value(){
@@ -108,8 +119,8 @@ float force_sensor_value(){
         force = (fsrG - 0.00075) / 0.00000032639;
       else
         force =  fsrG / 0.000000642857;
-      Serial.println("Force: " + String(force) + " g");
-      Serial.println();
+//      Serial.println("Force: " + String(force) + " g");
+//      Serial.println();
       return force; 
     }
     else{
@@ -196,36 +207,98 @@ float imu_data(){
       myIMU.yaw   -= 8.5;
       myIMU.roll  *= RAD_TO_DEG;
 
-      Serial.print("Yaw, Pitch, Roll: ");
-      Serial.print(myIMU.yaw, 2);
-      Serial.print(", ");
-      Serial.print(myIMU.pitch, 2);
-      Serial.print(", ");
-      Serial.println(myIMU.roll, 2);
+//      Serial.print("yaw: ");
+//      Serial.println(myIMU.yaw, 2);
+    //Serial.print("pitch: ");
+    //Serial.println(myIMU.pitch+180, 2);
+//      Serial.print("roll: ");
+//      Serial.println(myIMU.roll, 2);
+//      Serial.println(" ");
       
-      //sends a value through bluetooth
-      bluetooth.println(myIMU.roll);
+//      //sends a value through bluetooth
+//      bluetooth.println(myIMU.pitch);
       
       myIMU.count = millis();
       myIMU.sumCount = 0;
       myIMU.sum = 0;
     } // if (myIMU.delt_t > 500)
-    return myIMU.roll;
+    return myIMU.pitch+180;
   }
+
+void  change_motor(int current_position){
+     if (current_position == 1){
+        for (pos = 45; pos <= 110; pos += 1) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+        }
+      }
+     else{
+                 for (pos = 110; pos >= 45; pos -= 1) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+        }
+      }
+
+  }
+  struct Data {
+  float bottom, top;
+};
+struct Data  calibrate(){
+    int var = 0; 
+    int var2 = 0; 
+    float average_bottom = 0;
+    float average_top = 0;
+    Serial.println("calibrating hold for 5s");
+    while (var< 2000){
+      average_bottom = average_bottom+ imu_data();
+      delay(1);
+      var++;
+
+      }
+      Serial.println("hold in top position");
+      delay(5000);
+      while (var2< 2000){
+      average_top = average_top+ imu_data();
+      delay(1);
+      var2++;
+
+      }
+      Serial.println("done calibration results:");
+      average_bottom = average_bottom/2000;
+      average_top = average_top/2000;
+      return {average_bottom, average_top};
+  }
+bool start = true; 
+float imu_offset;
+struct Data stuff;
 void loop()
 {
-
+  if (start == true){
+      stuff =calibrate(); 
+      Serial.println(stuff.bottom);
+      Serial.println(stuff.top);
+      imu_offset =imu_data();
+      Serial.println("sample calculation");
+      Serial.println(imu_offset);
+      Serial.println((imu_offset- stuff.bottom)/(stuff.top-stuff.bottom));
+      start =false;
+    }
   // checks if the bluetooth sent any characters
     if(bluetooth.available())  // If ths bluetooth sent any characters
   {
     // prints out the values sent by the bluetooth module
     blue = bluetooth.read();
-    if (blue== 1 || blue== 0){
-       Serial.print(blue);  
+    change_motor(blue);
+    //bluetooth.println(5);  
        //sends commsnd to lock the glove locks the glove
-      }
   }
-   bluetooth.println(force_sensor_value());
-   bluetooth.println(imu_data());
+//  //sends a value through bluetooth
+//   //bluetooth.println(force_sensor_value());
+//   Serial.println(imu_data());
+    Serial.println((imu_data()- stuff.bottom)/(stuff.top-stuff.bottom));
+   bluetooth.println((imu_data()- stuff.bottom)/(stuff.top-stuff.bottom) );
+   delay(10);
 
 }
--- python/virtual_joystick_windows.py
+++ python/virtual_joystick_windows.py
@@ -7,28 +7,46 @@ import struct
 from win32api import GetKeyState
 from win32con import VK_CAPITAL
 # starts serial communications on the bluetooth port
-ser = serial.Serial('COM15', 9600, timeout=1)
+ser = serial.Serial('COM17', 9600, timeout=10)
 print("\nConnected")
 
 MyVirtual = pyxinput.vController()
 MyRead = pyxinput.rController(1) # For Controller 1
 inp ="a"
+capslock_state_previous = 0
+count = 1
 while(True):
     try:
         # Read serial data
         s = ser.readline()
-        # s = s.decode("utf-8")
-
-        if s !=" ":
-            thumb = float(s)/100
+        q = s.decode("utf-8")
+        if q !='' and q != 0:
+            print("in")
+            .15
+            -.19
+            thumb = float(s)
+            # if thumb < 0.0:
+            #     thumb = thumb* -0.0
             print(thumb)
-            MyVirtual.set_value('AxisLx', thumb)
-            print(MyRead.gamepad)
+            MyVirtual.set_value('AxisLy', thumb*-1)
+            #print(MyRead.gamepad)
         # checks to see if the current capslock state is duplicated
         capslock_state = GetKeyState(VK_CAPITAL)
-        if capslock_state != capslock_state_previous:
+
+        if capslock_state != capslock_state_previous and capslock_state >= 0:
+            # print("sending")
+            # print(capslock_state)
             ser.write(struct.pack('>B',capslock_state))
-            capslock_state_previous = capslock_state
+            time.sleep(1)
+            J = ser.readline()
+            count += 1
+            # print("rec")
+            # print(J)
+        elif count == 2 and capslock_state == capslock_state_previous and capslock_state >= 0:
+            ser.write(struct.pack('>B',capslock_state))
+            time.sleep(1)
+            count = 1
+        capslock_state_previous = capslock_state
     # Allow program to be manually stopped
     except KeyboardInterrupt:
         print("Keyboard Interrupt")
