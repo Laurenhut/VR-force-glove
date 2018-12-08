81d974a832414bbe386b5c2ac098840aae4c439e
unknown <l.hutson@live.com>
2018-12-08 05:19:38 -0600

    arduino/unit tests/index_motor/index_motor.ino  Please enter the commit message for yq#hanges. Lines starting

--- arduino/unit tests/index_motor/index_motor.ino
+++ arduino/unit tests/index_motor/index_motor.ino
@@ -23,37 +23,36 @@ void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   pinMode(rightbuttonpin,INPUT);
   pinMode(leftbuttonpin,INPUT);
-  myservo.write(70); 
-
+  myservo.write(120); 
+ //myservo.write(150); 
 }
 
 void loop() {
-  stateright = digitalRead(rightbuttonpin);
-  stateleft = digitalRead(leftbuttonpin);
-
-  if (stateright == HIGH){
-      if (triggered == false){
-        for (pos = 45; pos <= 70; pos += 1) { // goes from 0 degrees to 180 degrees
-              // in steps of 1 degree
-              myservo.write(pos);              // tell servo to go to position in variable 'pos'
-              delay(15);                       // waits 15ms for the servo to reach the position
-        }
-        triggered =true;
-      }
-    }
-  else if (stateleft == HIGH){
-        if (triggered == false){
-           for (pos = 70; pos >= 45; pos -= 1) { // goes from 0 degrees to 180 degrees
-                // in steps of 1 degree
-                myservo.write(pos);              // tell servo to go to position in variable 'pos'
-                delay(15);                       // waits 15ms for the servo to reach the position
-            }
-        }
-        triggered =true;
-    }
-  else if (stateleft == LOW && stateright == LOW){
-         triggered = false;
-    }
+//  stateright = digitalRead(rightbuttonpin);
+//  stateleft = digitalRead(leftbuttonpin);
+//
+//  if (stateright == HIGH){
+//      if (triggered == false){
+//        for (pos = 45; pos <= 70; pos += 1) { // goes from 0 degrees to 180 degrees
+//              // in steps of 1 degree
+//              myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//              delay(15);                       // waits 15ms for the servo to reach the position
+//        }
+//        triggered =true;
+//      }
+//    }
+//  else if (stateleft == HIGH){
+//        if (triggered == false){
+//           for (pos = 70; pos >= 45; pos -= 1) { // goes from 0 degrees to 180 degrees
+//                // in steps of 1 degree
+//                myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//                delay(15);                       // waits 15ms for the servo to reach the position
+//            }
+//        }
+//        triggered =true;
+//    }
+//  else if (stateleft == LOW && stateright == LOW){
+//         triggered = false;
+//    }
  
 }
-
